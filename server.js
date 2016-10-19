bd54c4a7c97e3158e6b2438bf976b781ab3a5646
Lance Gonzalez <Lance.gonzalez7@Gmail.com>
2016-10-18 21:14:29 -0600

    finished

--- server.js
+++ server.js
@@ -1,1 +1,27 @@
+const express = require('express');
+const cors = require('cors');
+const body = require('body-parser');
 
+const app = express()
+
+app.use(cors());
+app.use(body.json());
+
+app.listen(8080, () => {
+    console.log('Listening on port 8080');
+});
+
+var messages = [];
+
+app.get('/', (req, res) => {
+    res.send(JSON.stringify(messages));
+});
+
+app.post('/', (req, res) => {
+    messages.push({
+        message: req.body.message,
+        time: new Date()
+    });
+    console.log(req.body.message);
+    res.send(JSON.stringify(messages));
+});
